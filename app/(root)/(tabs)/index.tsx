a419e04f5a0ffb33487f9c60d2e8a8c2e1d129a0
Fernando Castrejon <fernandocastrejonh@gmail.com>
2025-05-18 20:00:39 -0600

    changes

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -6,11 +6,13 @@ export default function Index() {
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
-      <Text className="font-bold text-lg my-10">Welcome to ReState</Text>
+      <Text className="font-bold my-10 font-rubik text-3xl">
+        Welcome to ReState
+      </Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
